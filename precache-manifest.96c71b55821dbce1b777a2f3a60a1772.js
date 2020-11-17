bbaefd59c1fc4911d57928133fb7e8f02df3ebfc
whiteartur1997 <whiteartur1997@gmail.com>
2020-11-17 12:26:55 +0200

    Updates

---
+++ precache-manifest.96c71b55821dbce1b777a2f3a60a1772.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ed6ee889b6ce453841f8baf724babe",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "f8061d8591a3d7c0f373",
+    "url": "/social-network/static/css/main.d40c0a3d.chunk.css"
+  },
+  {
+    "revision": "0c9f7952e8f23a51988d",
+    "url": "/social-network/static/js/2.e071113c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.e071113c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8061d8591a3d7c0f373",
+    "url": "/social-network/static/js/main.137916a7.chunk.js"
+  },
+  {
+    "revision": "6657823949f91321416d",
+    "url": "/social-network/static/js/runtime-main.005f4cf5.js"
+  },
+  {
+    "revision": "8758a83995b858b01b4db46b4e2680e1",
+    "url": "/social-network/static/media/batman.8758a839.png"
+  },
+  {
+    "revision": "4d76cfcdd8de4f3b07657035da00578c",
+    "url": "/social-network/static/media/cover.4d76cfcd.jpg"
+  },
+  {
+    "revision": "aa5b7491772cb24c710b53090bc68564",
+    "url": "/social-network/static/media/discoGirl.aa5b7491.png"
+  },
+  {
+    "revision": "9d1202b0773af3386329471593ef4738",
+    "url": "/social-network/static/media/doctorWho.9d1202b0.png"
+  },
+  {
+    "revision": "8631c42599518167916a097412234456",
+    "url": "/social-network/static/media/einstein.8631c425.png"
+  },
+  {
+    "revision": "6e7f348cb008af0e91b993af9777a993",
+    "url": "/social-network/static/media/hakimAlAsad.6e7f348c.png"
+  },
+  {
+    "revision": "1923a86d8cee364bf334808097590862",
+    "url": "/social-network/static/media/johnDoe.1923a86d.png"
+  },
+  {
+    "revision": "71838e9967d4abe61d162781bd043c24",
+    "url": "/social-network/static/media/keanuReeves.71838e99.png"
+  },
+  {
+    "revision": "4060eaaddf6f455b25f641f33762eb85",
+    "url": "/social-network/static/media/kurtCobain.4060eaad.png"
+  },
+  {
+    "revision": "38dfc36c5f06c731534a4e0156e2bda4",
+    "url": "/social-network/static/media/margoRobby.38dfc36c.png"
+  },
+  {
+    "revision": "1ace64cb8123e34acf2e128430725061",
+    "url": "/social-network/static/media/markTwain.1ace64cb.png"
+  },
+  {
+    "revision": "104706619ad99f2b82e2bf4f525625d7",
+    "url": "/social-network/static/media/msFitcher.10470661.png"
+  },
+  {
+    "revision": "28a40bdc634cf2475a3006a0c3ed21ee",
+    "url": "/social-network/static/media/nelsonMandela.28a40bdc.png"
+  },
+  {
+    "revision": "3517a71b0bc69b0b881b849766325cb9",
+    "url": "/social-network/static/media/steveJobs.3517a71b.png"
+  },
+  {
+    "revision": "68b7cd7eb7b735871cc98176f12a99b6",
+    "url": "/social-network/static/media/suarez.68b7cd7e.png"
+  }
+]);
