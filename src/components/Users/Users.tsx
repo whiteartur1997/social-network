2ad425426978a920a7f75d9f9f31b805c03e5a76
whiteartur1997 <whiteartur1997@gmail.com>
2020-12-13 15:25:35 +0200

    after 53 lesson. axios get inside constructor

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios'; // уже написана на typescript
+import axios from 'axios';
 import React from 'react';
 import { UserType } from '../../redux/usersReducer';
 import UserCard from './UserCard/UserCard';
@@ -18,36 +18,38 @@ type GetUsersRepsonseType = {
   error: string
 }
 
-const Users = (props: UsersPropsType) => {
-  let getUser = () => {
-    if (props.users.length === 0) {
-      axios.get<GetUsersRepsonseType>("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-        props.setUsers(response.data.items);
-      }).catch(() => {
-        props.setUsers([]);
-      })
-    }
+class Users extends React.Component<UsersPropsType> {
+  // можно не писать это делегирование, 
+  // так как полностью строимся от родитея React.Component
+  constructor(props: UsersPropsType) {
+    alert("New")
+    super(props);
+    axios.get<GetUsersRepsonseType>("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+      this.props.setUsers(response.data.items);
+    }).catch(() => {
+      this.props.setUsers([]);
+    })
   }
 
-  return (
-    <div className={s.usersCards}>
-      <button onClick={getUser}>Get Users</button>
-      { props.users.map(u => {
-        return (
-          <UserCard
-            key={u.id}
-            user={u}
-            followUser={props.followUser}
-            unfollowUser={props.unfollowUser}
-          />
-        )
-      })}
-    </div>
-  )
+  render() {
+    return (
+      <div className={s.usersCards}>
+        { this.props.users.map(u => {
+          return (
+            <UserCard
+              key={u.id}
+              user={u}
+              followUser={this.props.followUser}
+              unfollowUser={this.props.unfollowUser}
+            />
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default Users;
-
 
 // {
   //   id: 1,
